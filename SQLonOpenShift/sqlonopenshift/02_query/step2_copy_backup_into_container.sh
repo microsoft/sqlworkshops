91de73ee1c491544303f2a2f235fb7b26edeb98e
rgward <thewandog@troyryanwin10.northamerica.corp.microsoft.com>
2019-04-09 21:34:36 -0500

    added file to retrieve wwi from the internet and fixed up scripts

--- SQLonOpenShift/sqlonopenshift/02_query/step2_copy_backup_into_container.sh
+++ SQLonOpenShift/sqlonopenshift/02_query/step2_copy_backup_into_container.sh
@@ -1,1 +1,3 @@
-oc rsync WideWorldImporters-Full.bak <pod>:/var/opt/mssql
+POD=$(oc get pods | grep mssql-deployment | awk {'print $1'})
+echo $POD
+oc cp ./WideWorldImporters-Full.bak $POD:/var/opt/mssql/WideWorldImporters-Full.bak
--- SQLonOpenShift/sqlonopenshift/02_query/step3_restore_backup.sh
+++ SQLonOpenShift/sqlonopenshift/02_query/step3_restore_backup.sh
@@ -2,4 +2,4 @@ SERVERIP=$(oc get service | grep mssql-service | awk {'print $4'})
 PORT=31433
 echo $SERVERIP
 echo $PORT
-sqlcmd -Usa -PSql2019isfast -S$SERVERIP,$PORT -irestore_wwi.sql
+sqlcmd -Usa -PSql2019isfast -S$SERVERIP,$PORT -irestorewwi.sql
