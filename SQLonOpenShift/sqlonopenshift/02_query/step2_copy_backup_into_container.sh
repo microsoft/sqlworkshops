6249e346e7ae95b97ec722de8de9616b8f9557bf
Bob Ward <bobward@microsoft.com>
2019-04-11 22:47:38 -0500

    changes for Module 2 scripts

--- SQLonOpenShift/sqlonopenshift/02_query/step2_copy_backup_into_container.sh
+++ SQLonOpenShift/sqlonopenshift/02_query/step2_copy_backup_into_container.sh
@@ -1,3 +1,2 @@
 POD=$(oc get pods | grep mssql-deployment | awk {'print $1'})
-echo $POD
 oc cp ./WideWorldImporters-Full.bak $POD:/var/opt/mssql/WideWorldImporters-Full.bak
--- SQLonOpenShift/sqlonopenshift/02_query/step3_restore_backup.sh
+++ SQLonOpenShift/sqlonopenshift/02_query/step3_restore_backup.sh
@@ -1,5 +1,3 @@
 SERVERIP=$(oc get service | grep mssql-service | awk {'print $4'})
 PORT=31433
-echo $SERVERIP
-echo $PORT
 sqlcmd -Usa -PSql2019isfast -S$SERVERIP,$PORT -irestorewwi.sql
