e77561eb6ec92813d8fa5c594b96076bad571661
Bob Ward <bobward@microsoft.com>
2019-08-22 16:12:26 -0500

    Update setup_classification.sql
    small fix found by Aaron Bertrand

--- sql2019lab/02_Security/dataclassification/setup_classification.sql
+++ sql2019lab/02_Security/dataclassification/setup_classification.sql
@@ -2,9 +2,13 @@
 USE WideWorldImporters
 GO
 IF EXISTS (SELECT * FROM sys.sensitivity_classifications sc WHERE object_id('[Application].[PaymentMethods]') = sc.major_id)
+BEGIN
 	DROP SENSITIVITY CLASSIFICATION FROM [Application].[PaymentMethods].[PaymentMethodName]
+END
 GO
 IF EXISTS (SELECT * FROM sys.sensitivity_classifications sc WHERE object_id('[Application].[People]') = sc.major_id)
+BEGIN
 	DROP SENSITIVITY CLASSIFICATION FROM [Application].[People].[FullName]
 	DROP SENSITIVITY CLASSIFICATION FROM [Application].[People].[EmailAddress]
+END
 GO
