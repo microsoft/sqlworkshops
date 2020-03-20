1cd0b54831352d39fa098a71846d27b283707cf6
Anna Hoffman <antho@microsoft.com>
2020-03-20 11:07:58 -0700

    update readme for azuresqlworkshop

--- AzureSQLWorkshop/azuresqlworkshop/04-Performance/monitor_and_scale/get_service_objective.sql
+++
@@ -1,5 +0,0 @@
-SELECT�database_name,slo_name,cpu_limit,max_db_memory, max_db_max_size_in_mb,�primary_max_log_rate,primary_group_max_io,�volume_local_iops,volume_pfs_iops
-FROM�sys.dm_user_db_resource_governance;
-GO
-SELECT DATABASEPROPERTYEX('AdventureWorks<ID>', 'ServiceObjective');
-GO
--- AzureSQLWorkshop/azuresqlworkshop/04-Performance/tuning_applications/order_rating_ddl.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS SalesLT.OrderRating;
-GO
-CREATE TABLE SalesLT.OrderRating
-(OrderRatingID int identity not null,
-SalesOrderID int not null,
-OrderRatingDT datetime not null,
-OrderRating int not null,
-OrderRatingComments char(500) not null);
-GO
